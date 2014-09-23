7718107a2769b0ed0415a827b0dc2effd1b55fa7
Edwin Corzo <edwin11ramirez@gmail.com>
2014-09-22 19:25:07 -0700

    made a transparent box

--- css/main.js
+++ css/main.js
@@ -8,3 +8,13 @@ $(window).scroll(function() {
 
 
 
+$('.menu').hide(0);
+
+$('.header p').click(function() {
+  $('.menu').toggle(0);
+  $('.header-extra').toggle(0);
+  $('.menu li').click(function(){
+    $('.menu').hide(0);
+    $('.header-extra').show(0);
+  });
+});
