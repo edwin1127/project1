91c4f0f45b8893630be0d7f3344bec0dd5243586
Edwin Corzo <edwin11ramirez@gmail.com>
2014-09-24 11:39:17 -0700

    Added an image grid

--- css/main.js
+++ css/main.js
@@ -1,9 +1,5 @@
 // Long form
-$(window).scroll(function() {
-    var x = jQuery(this).scrollTop(); // Pos
-    var r = 10; // Rate
-    $('body').css('background-position', 'center ' + parseInt(x/r) + 'px'); // Switch x polarity to reverse direction
-});
+
 
 
 
