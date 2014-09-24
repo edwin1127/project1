3459fd125c992f6de778ce3148b5c3edf8e86ce3
Edwin Corzo <edwin11ramirez@gmail.com>
2014-09-24 12:22:22 -0700

    Added another image grid

--- css/main.js
+++ css/main.js
@@ -3,14 +3,3 @@
 
 
 
-
-$('.menu').hide(0);
-
-$('.header p').click(function() {
-  $('.menu').toggle(0);
-  $('.header-extra').toggle(0);
-  $('.menu li').click(function(){
-    $('.menu').hide(0);
-    $('.header-extra').show(0);
-  });
-});
